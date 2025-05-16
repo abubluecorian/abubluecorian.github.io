597f722641ed8d8b1c87db77c0053c7a124e74c1
Muhammad Abu zer <abu786.japit@gmail.com>
2025-05-16 12:35:03 +0530

    16thmay change

--- mail/mail.php
+++ mail/mail.php
@@ -1,22 +1,34 @@
-<?
-$name = $_POST{'name'};
-$email = $_POST{'email'};
-$phone = $_POST{'phone'};
-$subject = $_POST{'subject'};
-$message = $_POST['message'];
-
-$email_message = "
-
-Name: ".$name."
-Email: ".$email."
-Phone: ".$phone."
-Subject: ".$subject."
-Message: ".$message."
-
-";
-
-mail ("abu786.japit@gmail.com" , "New Message", $email_message);
-header("location: ../mail-success.html");
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $to = "hostingabu@gmail.com";
+    $subject = "New Form Submission";
+    
+    // Collect form data
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $phone = $_POST['phone'];
+    $date = $_POST['date'];
+    $message = $_POST['message'];
+    
+    // Create email body
+    $body = "Name: $name\n";
+    $body .= "Email: $email\n";
+    $body .= "Phone: $phone\n";
+    $body .= "Date: $date\n";
+    $body .= "Message:\n$message";
+    
+    // Set headers
+    $headers = "From: $email" . "\r\n" .
+               "Reply-To: $email" . "\r\n";
+    
+    // Send email
+    if (mail($to, $subject, $body, $headers)) {
+        // Success - redirect to thank you page
+        header('Location: thank-you.html');
+    } else {
+        echo "Error sending message";
+    }
+} else {
+    header("Location: your-form-page.html");
+}
 ?>
-
-
